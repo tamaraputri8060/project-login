fbf8f913925377ec6d56083c75e6cba055b6ba93
tamaraputri8060 <tamaraputri8060@gmail.com>
2025-09-26 17:49:49 +0700

    desain halaman login

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Form Login</h2>
+
+    <?php if (!empty($error)) { ?>
+        <p style="color:red;"><?php echo $error; ?></p>
+    <?php } ?>
+
+    <form method="post">
+        Username: <input type="text" name="username" required><br><br>
+        Password: <input type="password" name="password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
